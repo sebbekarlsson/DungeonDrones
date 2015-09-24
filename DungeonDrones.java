d7767adb3340ab921db9fdbec37ca3e078988bcd
Sebastian Karlsson <=>
2015-09-24 12:06:34 +0200

    Added main

---
+++ DungeonDrones.java
@@ -0,0 +1,21 @@
+package DungeonDrones;
+
+import org.lwjgl.LWJGLException;
+
+import seaviver.main.Scene;
+import seaviver.main.Seaviver;
+import seaviver.main.test.Test_Scene;
+
+public class DungeonDrones {
+	Seaviver engine;
+	
+	public DungeonDrones() throws LWJGLException{
+		engine = new Seaviver(new Scene[]{
+				new Test_Scene()
+		});
+	}
+	
+	public static void main(String[] args) throws LWJGLException{
+		new DungeonDrones();
+	}
+}
